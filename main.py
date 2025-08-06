be4f64289ce5d0f2f370af678cde18ae1d32bfca
JaoVicy <filipimjoa@gmail.com>
2025-08-06 19:07:44 -0300

    feat: created 2 more vars

--- main.py
+++ main.py
@@ -6,3 +6,5 @@ def main():
 
     diretorio_projeto = sys.argv[1]
     requirements_file = os.path.join(diretorio_projeto, 'requirements.txt')
+    criar_ambiente(diretorio_projeto)
+    instalar_dependencias(requirements_file)
