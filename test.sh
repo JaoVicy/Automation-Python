c747dea5e098c070e0e04e14d171b77dbba640be
JaoVicy <filipimjoa@gmail.com>
2025-08-13 19:06:17 -0300

    feat: added chrome_opitions

--- test.sh
+++ test.sh
@@ -3,3 +3,6 @@ import time
 from selenium import webdriver
 
 chrome_path = "/Users/jao/Documents/Projetos/Automation-Python/chrome-mac-arm64/Google Chrome for Testing.app/Contents/MacOS/Google Chrome for Testing"
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = chrome_path
